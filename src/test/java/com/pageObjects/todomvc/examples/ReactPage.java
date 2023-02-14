2a4fcc1556380104078f9c14c0c8ec9d83b97c4b
liammag <maguire.j.liam@gmail.com>
2023-02-13 22:39:10 -0500

    added reactpage object

---
+++ src/test/java/com/pageObjects/todomvc/examples/ReactPage.java
@@ -0,0 +1,101 @@
+package com.pageObjects.todomvc.examples;
+
+import static com.codeborne.selenide.Selenide.$;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+import com.codeborne.selenide.SelenideElement;
+import util.webdriver.BasePageActions;
+
+public class ReactPage extends BasePage {
+
+    @FindBy(className = "new-todo")
+    public SelenideElement todoTextField;
+
+    @FindBy(linkText = "All")
+    public SelenideElement allBtn;
+
+    @FindBy(linkText = "Active")
+    public SelenideElement activeBtn;
+
+    @FindBy(linkText = "Completed")
+    public SelenideElement completedBtn;
+
+    @FindBy(linkText = "destroy")
+    public SelenideElement xBtn;
+
+    @FindBy(className = "todo-list")
+    public List<SelenideElement> todoList;
+
+    @FindBy(className = "clear-completed")
+    public SelenideElement clearCompletedBtn;
+
+    public void clickOnTextField() {
+        BasePageActions.clickOnElement(todoTextField);
+    }
+
+    public void enterTextInToDoField(String text) {
+        BasePageActions.setDataToField(todoTextField, text);
+    }
+
+    public void pressReturnKeyOnTextField() {
+        BasePageActions.pressReturnKey(todoTextField);
+    }
+
+    public void clickOnAllBtn() {
+        BasePageActions.clickOnElement(allBtn);
+    }
+
+    public void clickOnActiveBtn() {
+        BasePageActions.clickOnElement(activeBtn);
+    }
+
+    public void clickOnCompletedBtn() {
+        BasePageActions.clickOnElement(completedBtn);
+    }
+
+    public void clickOnXBtn() {
+        BasePageActions.clickOnElement(xBtn);
+    }
+
+    public void clickOnClearCompletedBtn() {
+        BasePageActions.clickOnElement(completedBtn);
+    }
+
+    public void clickOnCompletedRadioBtn() {
+        BasePageActions.clickOnElement(completedBtn);
+    }
+
+    public void enterTextInToDoFieldAndReturn(String text) {
+        clickOnTextField();
+        enterTextInToDoField(text);
+        pressReturnKeyOnTextField();
+    }
+
+    public String getLastListItem() {
+        String blah = todoList.get(todoList.size() - 1).getText();
+
+        return blah;
+    }
+
+    public String getValueOfListItem() {
+        String blah = todoList.get(todoList.size() - 1).getText();
+
+        return blah;
+    }
+
+    public boolean checkTextInList(String text) {
+
+        String xpath = String.format("//*[contains(text(),'initially open')]", text);
+        return todoList.contains(xpath);
+    }
+
+    public void editToDoItem(String oldValue, String newValue) {
+
+        SelenideElement todoField = $(By.xpath("/html/body/section/div/section/ul/li/div/label"));
+        BasePageActions.doubleClickOnElementAndEnterText(todoField, newValue);
+        
+    }
+}
